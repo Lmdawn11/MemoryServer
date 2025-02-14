cec35a737e2ef7cba37310b3a4a3dbd63229d70e
ming <877598289@qq.com>
2025-02-14 20:24:33 +0800

    消息初始化

--- src/main/java/com/ming/message/Message.java
+++ src/main/java/com/ming/message/Message.java
@@ -21,25 +21,14 @@ public abstract class Message implements Serializable {
 
     public static final int LoginRequestMessage = 0;
     public static final int LoginResponseMessage = 1;
-    public static final int ChatRequestMessage = 2;
-    public static final int ChatResponseMessage = 3;
-    public static final int GroupCreateRequestMessage = 4;
-    public static final int GroupCreateResponseMessage = 5;
-    public static final int GroupJoinRequestMessage = 6;
-    public static final int GroupJoinResponseMessage = 7;
-    public static final int GroupQuitRequestMessage = 8;
-    public static final int GroupQuitResponseMessage = 9;
-    public static final int GroupChatRequestMessage = 10;
-    public static final int GroupChatResponseMessage = 11;
-    public static final int GroupMembersRequestMessage = 12;
-    public static final int GroupMembersResponseMessage = 13;
-    public static final int PingMessage = 14;
-    public static final int RPC_MESSAGE_TYPE_REQUEST = 101;
-    public static final int RPC_MESSAGE_TYPE_RESPONSE = 102;
+    public static final int SetRequestMessage = 2;
+    public static final int SetResponseMessage = 3;
     private static final Map<Integer, Class<?>> messageClasses = new HashMap<>();
 
     static {
         messageClasses.put(LoginRequestMessage, LoginRequestMessage.class);
         messageClasses.put(LoginResponseMessage, LoginResponseMessage.class);
+        messageClasses.put(SetRequestMessage, SetRequestMessage.class);
+        messageClasses.put(SetResponseMessage, SetResponseMessage.class);
     }
 }
